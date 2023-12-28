5edbeb556a7a1908620269cd42d1de0efc96af85
a-marcos <82239362+a-marcos@users.noreply.github.com>
2023-12-28 12:22:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true })); // Para parsear dados de formulário
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+// Rota para lidar com o cadastro de usuários
+app.post('/cadastro', (req, res) => {
+  const { nome, email, senha } = req.body;
+  // Aqui você pode tratar os dados, como salvá-los em um banco de dados, por exemplo
+  console.log(`Novo cadastro: Nome - ${nome}, Email - ${email}, Senha - ${senha}`);
+  // Pode redirecionar o usuário para outra página após o cadastro ser processado
+  res.redirect('/obrigado');
 });
 
 app.listen(port, () => {
